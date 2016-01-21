a2b69ef1e22ba0f86a7bff421dc72058870151c2
Brenton Leanhardt <bleanhar@redhat.com>
2016-01-21 09:53:29 -0500

    pu

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ get '/' do
     <br /><br />check out your <a href=\"/agent\">user_agent</a>"
 end
 
+get '/user' do
+  "the user this application is running as is #{ENV['USER']}"
+end
+
 get '/agent' do
   "you're using #{request.user_agent}"
 end
